6a8445fb9ffddff5a2a4dae214f042e32330bd31
Djordje Djordjevic <dd20200077@student.fon.bg.ac.rs>
2024-03-10 22:47:19 +0100

    Dodata klasa koja implementira Interfejs

---
+++ ZadatakGit/src/implmentacijainterfejsa/Implementacija.java
@@ -0,0 +1,26 @@
+package implmentacijainterfejsa;
+
+import interfejspaket.Interfejs;
+import magacin.Artikal;
+
+public class Implementacija implements Interfejs {
+
+	@Override
+	public void dodaj(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaci(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pronadji(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
